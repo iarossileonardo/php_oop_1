e61d6c9b2da6ab628bb0d613cbddb728ccb2f11b
lai2-24 <lai2-24@itismeucci.com>
2025-02-12 08:44:50 +0100

    alunno finito

--- es1/alunno.php
+++ es1/alunno.php
@@ -1,11 +1,41 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    
-</body>
-</html>
+<?php
+    class Alunno {
+        protected $nome;
+        protected $cognome;
+        protected $eta;
+    }
+
+    function __construct($nome, $cognome, $eta) {
+        $this->nome = $nome;
+        $this->cognome = $cognome;
+        $this->eta = $eta;
+    }
+
+    function get_nome() {
+        return $this->nome;
+    }
+
+    function get_cognome() {
+        return $this->cognome;
+    }
+
+    function get_eta() {
+        return $this->eta;
+    }
+
+    function set_nome() {
+        $this->nome;
+    }
+
+    function set_cognome() {
+        $this->cognome;
+    }
+
+    function set_eta() {
+        $this->eta;
+    }
+
+    function stampaAlunno() {
+        echo "{$nome} {$cognome}, {$eta} anni."
+    }
+?>
